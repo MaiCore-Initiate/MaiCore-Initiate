e23cb29345e3d1dcdb0bf4926f7b6f619898ce12
盛梓城 <xiaochengzhixue@outlook.com>
2025-06-26 16:40:48 +0800

    更新MongoDB服务安装脚本，安装8.0版

--- MaiBotLauncher-v3.4.2-Linux调试版.py
+++ MaiBotLauncher-v3.4.2-Linux调试版.py
@@ -722,9 +722,9 @@ def install_mongodb_service():
         'sudo apt update',
         'sudo apt install -y wget gnupg',
         # 使用新的GPG密钥安装方式
-        'wget -qO- https://www.mongodb.org/static/pgp/server-7.0.asc | gpg --dearmor | sudo tee /usr/share/keyrings/mongodb-server-7.0.gpg > /dev/null',
+        'wget -qO- https://www.mongodb.org/static/pgp/server-8.0.asc | gpg --dearmor | sudo tee /usr/share/keyrings/mongodb-server-8.0.gpg > /dev/null',
         # 添加带签名的源
-        'echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list',
+        'echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list',
         'sudo apt update',
         'sudo apt install -y mongodb-org',
         'sudo systemctl start mongod',
